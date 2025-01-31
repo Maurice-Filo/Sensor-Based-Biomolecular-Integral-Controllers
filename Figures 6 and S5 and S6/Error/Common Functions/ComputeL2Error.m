23c7231e6d4bf26213c7cd24fbc4d79edb2ab418
Maurice Filo <99184019+Maurice-Filo@users.noreply.github.com>
2025-01-31 15:28:15 +0100

    Delete Figures 6 and S5 and S6/Error directory

--- Figures 6 and S5 and S6/Error/Common Functions/ComputeL2Error.m
+++
@@ -1,8 +0,0 @@
-function L2_norm = ComputeL2Error(y, t, r)
-    x = y - r;
-    if length(x) ~= length(t)
-        error('Signal and time vectors must have the same length');
-    end
-    dt = diff(t);
-    L2_norm = sqrt(sum((x(1:end-1).^2) .* dt));
-end
